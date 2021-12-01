2f45269800a17514358ea64116f799a402117707
Hilko Wiards <hilko@local>
2021-12-01 17:55:07 +0100

    cleanup day01

--- day01/day01.py
+++ day01/day01.py
@@ -1,13 +1,13 @@
 from helper import *
 import numpy as np
 
-def part1(depths: str):
+def part1(depths):
     diffs = np.diff(depths)
     num_pos = np.sum(diffs > 0, axis=0)
     print(num_pos)
     return num_pos
 
-def part2(depths:str):
+def part2(depths):
     window = depths[:-2] + depths[1:-1] + depths[2:]
     return part1(window)
 
